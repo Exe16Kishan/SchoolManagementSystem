42b9ca15cb053212a9c017610c22baf73d71ebc3
Exe16Kishan <kishanshukla53@gmail.com>
2026-09-19 16:54:49 +0530

    added student and teacher class

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-type Class = {
+type ClassType = {
   name: number;
-  students: Student[];
-  teacher: Teacher[];
-  subjects: Subject[];
+  students: StudentType[];
+  teacher: TeacherType[];
+  subjects: SubjectType[];
 };
 
-type Student = {
+type StudentType = {
   studentId: string;
   name: string;
   age: number;
   class: number;
-  attendance: number;
 };
 
-type Teacher = {
+type TeacherType = {
   teacherId: number;
   name: string;
   subjects: string[];
   classes: number[]; // class names are numbers
 };
 
-type Subject = {
+type SubjectType = {
   subjectId: string;
   subjectName: string;
   Teacher: string;
@@ -30,10 +29,10 @@ type Subject = {
 class School {
   private static schoolInstance: School;
 
-  classes: Class[] = [];
-  students: Student[] = [];
-  teachers: Teacher[] = [];
-  subjects: Subject[] = [];
+  classes: ClassType[] = [];
+  students: StudentType[] = [];
+  teachers: TeacherType[] = [];
+  subjects: SubjectType[] = [];
   private constructor() {}
 
   static instance(): School {
@@ -42,11 +41,72 @@ class School {
     }
     return this.schoolInstance;
   }
+
+  addStudent(studentDetail: Student): void {
+    this.students.push(studentDetail);
+  }
+
+  removeStudent(studentId: string) {
+    let index = this.students.findIndex(
+      (student) => student.studentId == studentId,
+    );
+    this.students.splice(index, 1);
+  }
 }
 
+class Student {
+  studentId: string;
+  name: string;
+  age: number;
+  class: number;
+  constructor({ studentId, age, class: std, name }: Student) {
+    ((this.studentId = studentId),
+      (this.name = name),
+      (this.age = age),
+      (this.class = std));
+  }
 
+  // like we can add some methods later
+}
 
-const school1 = School.instance()
+class Teacher {
+  teacherId: number;
+  name: string;
+  subjects: string[];
+  classes: number[];
 
+  constructor({classes,name,subjects,teacherId}:TeacherType){
+    this.teacherId = teacherId,
+    this.name = name ,
+    this.subjects = subjects ,
+    this.classes = classes
+  }
+}
 
-console.log(school1)
+// dummy data jsut to check
+let student1: Student = {
+  studentId: "1234",
+  name: "kishan",
+  age: 12,
+  class: 6,
+};
+
+let student2: Student = {
+  studentId: "5678",
+  name: "sonal",
+  age: 12,
+  class: 6,
+};
+const school1 = School.instance();
+
+// add students
+school1.addStudent(student1);
+school1.addStudent(student2);
+
+// remove students
+school1.removeStudent("5678"); // its working
+
+console.log(school1);
+
+// lets cleanup and use other classes as well
+// like we are creating other obejcts using class
